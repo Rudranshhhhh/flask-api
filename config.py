8bceeeb425c011e6ce6274bbb50b8013eecd5945
rajesh <rajesh199@gmail.com>
2024-12-22 12:47:21 +0530

    time increase

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_default_secret_key'
     MONGO_URI = os.environ.get('MONGO_URI') or 'mongodb://localhost:27017/your_database_name'
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'your_jwt_secret_key'
-    PASSWORD_RESET_EXPIRATION = 3600  # 1 hour in seconds
+    PASSWORD_RESET_EXPIRATION = 7200  # 2 hour in seconds
